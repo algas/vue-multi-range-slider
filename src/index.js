d12daea01680e68173d06f5ffe89e1d0db02921e
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2019-04-09 19:45:24 +0900

    Update

--- src/index.js
+++ src/index.js
@@ -7,32 +7,47 @@ let app = new Vue({
     apexchart: VueApexCharts,
   },
   data: {
+    selectedDataPoints: [-1],
+    seriesIndex: -1,
+    dataPointIndex: -1,
     series: [{
       name: 'Marine Sprite',
-      data: [44, 55, 41, 37, 22, 43, 21]
+      data: [44]
     }, {
       name: 'Striking Calf',
-      data: [53, 32, 33, 52, 13, 43, 32]
+      data: [53]
     }, {
       name: 'Tank Picture',
-      data: [12, 17, 11, 9, 15, 11, 20]
+      data: [12]
     }, {
       name: 'Bucket Slope',
-      data: [9, 7, 5, 8, 6, 9, 4]
+      data: [9]
     }, {
       name: 'Reborn Kid',
-      data: [25, 12, 19, 32, 25, 24, 10]
+      data: [25]
     }],
     chartOptions: {
       chart: {
         stacked: true,
-        stackType: '100%'
+        // stackType: '100%'
+        events: {
+          // click: function(ctx){
+          //   console.log(`event: click = ${JSON.stringify(ctx)}`);
+          // },
+          dataPointSelection: function(event, chartContext, config){
+            console.log(`event: dataPointSelection event = ${JSON.stringify(event)}`);
+            const keys = Object.keys(config.selectedDataPoints);
+            const ps = keys.map(function(k){return parseInt(k);})
+            console.log(`ps = ${ps}`);
+            console.log(chartContext);
+            app.selectedDataPoints = ps;
+          }
+        }
       },
       plotOptions: {
         bar: {
           horizontal: true,
         },
-
       },
       stroke: {
         width: 1,
@@ -43,25 +58,41 @@ let app = new Vue({
         text: '100% Stacked Bar'
       },
       xaxis: {
-        categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+        categories: [2008],
       },
 
       tooltip: {
-        y: {
-          formatter: function (val) {
-            return val + "K"
-          }
-        }
+        enabled: false
       },
       fill: {
         opacity: 1
-
+      },
+      states: {
+        active: {
+          allowMultipleDataPointsSelection: true
+        }
       },
 
       legend: {
         position: 'top',
         horizontalAlign: 'left',
-        offsetX: 40
+        offsetX: 40,
+        onItemClick: {
+          toggleDataSeries: false
+        },
+        onItemHover: {
+          highlightDataSeries: true
+        }
+      }
+    }
+  },
+  watch: {
+    selectedDataPoints: function(ps){
+      console.log('selectedDataPoints');
+      if(ps.length == 2 && ps[1] - ps[0] == 1){
+        const dataTotal = this.series[ps[0]].data[0] + this.series[ps[1]].data[0];
+        this.series[ps[0]].data[0] = dataTotal;
+        this.series.splice(ps[1], 1);
       }
     }
   }
